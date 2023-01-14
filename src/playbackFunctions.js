de5ffc9123db492bd5d3f3c793d4450e82b1cd4f
Kickblip <wyatt.zilker@gmail.com>
2023-01-13 18:22:54 -0600

    restructure playbackmanager

--- src/playbackFunctions.js
+++ src/playbackFunctions.js
@@ -1,4 +1,4 @@
-const _getQueue = async (token) => {
+const getQueue = async (token) => {
 
     const result = await fetch(`https://api.spotify.com/v1/me/player/queue`, {
         method: 'GET',
@@ -10,7 +10,7 @@ const _getQueue = async (token) => {
 
 }
 
-const _getPlaybackState = async (token) => {
+const getPlaybackState = async (token) => {
 
     const result = await fetch(`https://api.spotify.com/v1/me/player`, {
         method: 'GET',
@@ -26,19 +26,5 @@ const _getPlaybackState = async (token) => {
     };
 };
 
-const skipToNextTrack = async (token, target_device, track_id, offset_ms) => {
 
-    fetch(`https://api.spotify.com/v1/me/player/play${target_device && `?device_id=${target_device}`}`, {
-        method: "PUT",
-        body: JSON.stringify({ "uris": [`spotify:track:${track_id}`] }),
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    }).catch(e => console.error(e));
-
-    console.log(`skipped to ${track_id} on ${target_device}`);
-
-};
-
-
-module.exports = { skipToNextTrack, _getPlaybackState, _getQueue };
+module.exports = { getPlaybackState, getQueue };
