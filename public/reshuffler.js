b6916ebe32b1aadd2ad682f485edec0efb48c0e6
Kickblip <wyatt.zilker@gmail.com>
2023-04-18 17:10:56 -0500

    cosine similarity

--- public/reshuffler.js
+++ public/reshuffler.js
@@ -37,118 +37,71 @@ const organizeQueue = async (playback_queue) => {
         };
     });
 
+
+
     // store the features of the first song because it needs to remain constant
     const current_song = audio_features[0];
 
-    // extract the vectors from the audio features
-    const vectors = audio_features.map(feature => feature.vector);
-
-    // Define the number of clusters you want to use
-    const numClusters = 5;
-
-    // Define a function to calculate the Euclidean distance between two vectors
-    const euclideanDistance = (a, b) => {
-        let sum = 0;
-        for (let i = 0; i < a.length; i++) {
-            sum += (a[i] - b[i]) ** 2;
+    function cosineSimilarity(vectorA, vectorB) {
+        if (vectorA.length !== vectorB.length) {
+            throw new Error('Vectors must be of the same length');
         }
-        return Math.sqrt(sum);
+
+        let dotProduct = 0;
+        let magnitudeA = 0;
+        let magnitudeB = 0;
+
+        for (let i = 0; i < vectorA.length; i++) {
+            dotProduct += vectorA[i] * vectorB[i];
+            magnitudeA += vectorA[i] * vectorA[i];
+            magnitudeB += vectorB[i] * vectorB[i];
+        }
+
+        magnitudeA = Math.sqrt(magnitudeA);
+        magnitudeB = Math.sqrt(magnitudeB);
+
+        if (magnitudeA === 0 || magnitudeB === 0) {
+            return 0;
+        }
+
+        return dotProduct / (magnitudeA * magnitudeB);
     }
 
-    // Define a function to randomly initialize the cluster centroids
-    const initializeCentroids = (vectors, numClusters) => {
-        const centroids = [];
-        for (let i = 0; i < numClusters; i++) {
-            const randomIndex = Math.floor(Math.random() * vectors.length);
-            centroids.push(vectors[randomIndex]);
-        }
-        return centroids;
-    }
+    let queue = audio_features.slice(1);
+    let organized_queue = [current_song];
 
-    // Define a function to assign each vector to the closest cluster centroid
-    const assignToClusters = (vectors, centroids) => {
-        const clusters = Array.from({ length: numClusters }, () => []);
-        for (let i = 0; i < vectors.length; i++) {
-            let closestCentroid = null;
-            let closestDistance = Infinity;
-            for (let j = 0; j < centroids.length; j++) {
-                const distance = euclideanDistance(vectors[i], centroids[j]);
-                if (distance < closestDistance) {
-                    closestCentroid = j;
-                    closestDistance = distance;
-                }
-            }
-            clusters[closestCentroid].push(vectors[i]);
-        }
-        return clusters;
-    }
+    console.log(audio_features)
+    console.log(queue)
 
-    // Define a function to calculate the new cluster centroids based on the current assignments
-    const calculateNewCentroids = (clusters) => {
-        const centroids = [];
-        for (let i = 0; i < clusters.length; i++) {
-            if (clusters[i].length === 0) {
-                // If a cluster has no vectors, reinitialize its centroid randomly
-                centroids.push(initializeCentroids([clusters], 1)[0]);
-            } else {
-                const centroid = clusters[i][0].map((_, j) =>
-                    clusters[i].reduce((sum, vector) => sum + vector[j], 0) / clusters[i].length
-                );
-                centroids.push(centroid);
+
+    const organizeQueue = (queue, current_song, organized_queue = []) => {
+        if (queue.length === 0) {
+            return organized_queue;
+        }
+
+        let highest_similarity = 0;
+        let highest_similarity_index = 0;
+
+        for (let i = 0; i < queue.length; i++) {
+            const similarity = cosineSimilarity(current_song.vector, queue[i].vector);
+            if (similarity > highest_similarity) {
+                highest_similarity = similarity;
+                highest_similarity_index = i;
             }
         }
-        return centroids;
-    }
 
-    // Define a function to check if the cluster assignments have converged
-    const hasConverged = (oldAssignments, newAssignments) => {
-        for (let i = 0; i < oldAssignments.length; i++) {
-            if (oldAssignments[i].length !== newAssignments[i].length) {
-                return false;
-            }
-            for (let j = 0; j < oldAssignments[i].length; j++) {
-                if (oldAssignments[i][j] !== newAssignments[i][j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+        organized_queue.push(queue[highest_similarity_index]);
+        queue.splice(highest_similarity_index, 1);
 
-    // Run the K-means algorithm
-    // Run the K-means algorithm
-    let centroids = initializeCentroids(vectors, numClusters);
-    let assignments = [];
-    let converged = false;
-    while (!converged) {
-        assignments = assignToClusters(vectors, centroids);
-        const newCentroids = calculateNewCentroids(assignments);
-        converged = hasConverged(assignments, assignToClusters(vectors, newCentroids));
-        centroids = newCentroids;
-    }
+        return organizeQueue(queue, current_song, organized_queue);
+    };
+    organizeQueue(queue, current_song, organized_queue);
 
-    // Once the clustering is complete, sort the vectors within each cluster based on their distance to the centroid
-    for (let i = 0; i < assignments.length; i++) {
-        assignments[i].sort((a, b) => {
-            const distanceToCentroidA = euclideanDistance(a, centroids[i]);
-            const distanceToCentroidB = euclideanDistance(b, centroids[i]);
-            return distanceToCentroidA - distanceToCentroidB;
-        });
-    }
 
-    // Concatenate the vectors within each cluster to get your final sorted list
-    const rearrangedVectors = [current_song.vector]; // add the first song to the beginning
-    for (let i = 0; i < assignments.length; i++) {
-        const cluster = assignments[i];
-        for (let j = 0; j < cluster.length; j++) {
-            if (i === 0 && j === 0) {
-                continue; // skip the first song because it's already added
-            }
-            rearrangedVectors.push(cluster[j]);
-        }
-    }
+    console.log(organized_queue);
 
-    console.log(rearrangedVectors);
+
+
 
 };
 
