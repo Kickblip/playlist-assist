5f8303e88c3a2684d44e24066dfd60664d0456a9
Kickblip <wyatt.zilker@gmail.com>
2023-04-26 13:10:43 -0500

    normalise da vectorz b4 calcoolation

--- src/analysis/math.js
+++ src/analysis/math.js
@@ -1,10 +1,18 @@
-const euclideanDistance = (arr1, arr2) => {
+const euclideanDistance = (vector1, vector2) => {
     let result = 0;
-    for (let i = 0; i < arr1.length; i++) {
-        result += (arr1[i] - arr2[i]) ** 2;
+    for (let i = 0; i < vector1.length; i++) {
+        result += (vector1[i] - vector2[i]) ** 2;
     };
     return Math.sqrt(result);
 };
 
+const normalize = (vector) => {
+    const mean = vector.reduce((a, b) => a + b) / vector.length;
+    const stdDev = Math.sqrt(
+        vector.reduce((a, b) => a + Math.pow(b - mean, 2), 0) / (vector.length - 1)
+    );
+    return vector.map((val) => (val - mean) / stdDev);
+};
 
-module.exports = { euclideanDistance };
+
+module.exports = { euclideanDistance, normalize };
